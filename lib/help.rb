bb5bde57684d307ba22016f8b0b35f2864fb2ca7
Jeff Michels <michels.jeff@gmail.com>
2010-05-29 15:17:16 -0700

    added help

--- lib/help.rb
+++ lib/help.rb
@@ -1,12 +1,16 @@
 module Heroku::Command
 
   Help.group 'Tiketly Commands' do |group|
-    group.command 'tix:list', 'show tickets'
+    group.command 'tix:init', 'create username/password/initial project'    
+    group.command 'tix:collaborator_sync', 'sync collaborators on this project'
+    group.command 'tix', "see tix:all"
+    group.command 'tix:all', "search for tickets (--sort, --owner, --state, --tags set these options)"
+    group.command 'tix:create', "create a ticket (-t sets title, -d description, -s state, -p priority, --tags comma separated labels, --owner owner email)"
   end
 
   class Ticketly
     def help
-      puts "Help"
+      command("help")
     end
   end
 end
